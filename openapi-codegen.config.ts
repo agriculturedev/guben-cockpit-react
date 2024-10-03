c2e47ad6f6bf639331e618770f470589a253127a
Jonas Faber <jonas.h.faber@gmail.com>
2024-10-03 16:12:04 +0200

    add code gen config

---
+++ openapi-codegen.config.ts
@@ -0,0 +1,25 @@
+import {
+  generateSchemaTypes,
+  generateReactQueryComponents,
+} from "@openapi-codegen/typescript";
+import { defineConfig } from "@openapi-codegen/cli";
+export default defineConfig({
+  gubenProd: {
+    from: {
+      relativePath:
+        "../guben-cockpit-strapi/src/extensions/documentation/documentation/1.0.0/full_documentation.json",
+      source: "file",
+    },
+    outputDir: "endpoints",
+    to: async (context) => {
+      const filenamePrefix = "gubenProd";
+      const { schemasFiles } = await generateSchemaTypes(context, {
+        filenamePrefix,
+      });
+      await generateReactQueryComponents(context, {
+        filenamePrefix,
+        schemasFiles,
+      });
+    },
+  },
+});
