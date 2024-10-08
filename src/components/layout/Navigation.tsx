ac5fe04e0e1be2d2ed1a584d518882faeee77423
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2024-10-08 19:59:52 +0200

    update navigation to match guben design

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -4,6 +4,8 @@ import {HomeIcon, ListBulletIcon, MapIcon, Squares2X2Icon} from "@heroicons/reac
 
 
 export const Navigation = () => {
+    const linkStyle = "h-full p-3 flex items-center justify-center w-auto rounded-xl group hover:bg-[#cd1421]";
+
     return (
         <div className="w-full h-[80px] bg-white sticky top-0 z-10 shadow p-0 m-0 rounded-b flex flex-row items-center justify-between">
             <div id="logo" className="flex-1 flex justify-start items-start h-full pl-5">
@@ -13,22 +15,22 @@ export const Navigation = () => {
             </div>
             <ul className="flex-1 flex flex-row h-full items-center justify-center self-center">
                 <li>
-                    <Link to="/" className="h-full p-2 flex items-center justify-center w-auto rounded group hover:bg-[#cd1421]">
+                    <Link to="/" className={linkStyle}>
                         <HomeIcon className="icon h-[20px] w-[20px] text-[#cd1421] group-hover:text-white"/>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/projects" className="h-full p-2 flex items-center justify-center w-auto rounded group hover:bg-[#cd1421]">
+                    <Link to="/projects" className={linkStyle}>
                         <Squares2X2Icon className="icon h-[20px] w-[20px] text-[#cd1421] group-hover:text-white"/>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/map" className="h-full p-2 flex items-center justify-center w-auto rounded group hover:bg-[#cd1421]">
+                    <Link to="/map" className={linkStyle}>
                         <MapIcon className="icon h-[20px] w-[20px] text-[#cd1421] group-hover:text-white"/>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/events" className="h-full p-2 flex items-center justify-center w-auto rounded group hover:bg-[#cd1421]">
+                    <Link to="/events" className={linkStyle}>
                         <ListBulletIcon className="icon h-[20px] w-[20px] text-[#cd1421] group-hover:text-white"/>
                     </Link>
                 </li>
