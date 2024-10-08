b72ac68dd20f149bd2889a24707ac0f276ad6fb1
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2024-10-08 20:09:47 +0200

    optimize tabs a bit with card component and tab trigger styling

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react'
 import {createFileRoute} from '@tanstack/react-router'
-import {useGetHomeView, useGetProjects, useGetProjectView} from "@/endpoints/gubenProdComponents";
+import {useGetHomeView} from "@/endpoints/gubenProdComponents";
 import {View} from "@/components/layout/View";
-import {PaginationContainer} from "@/components/DataDisplay/PaginationContainer";
-import {usePagination} from "@/hooks/usePagination";
 import {DashboardTabs, TabItem} from "@/components/home/DashboardTabs";
 
 export const Route = createFileRoute('/')({
