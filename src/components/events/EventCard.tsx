a3b585c70a9419233b53b2404f88df4ad74addb3
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2024-10-13 22:44:39 +0200

    update event card formatting, only render categories when there are any

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -3,40 +3,44 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface EventCardProps {
-    event: EventListResponseDataItem;
+  event: EventListResponseDataItem;
 }
 
 export const EventCard = ({event}: EventCardProps) => {
 
-    const startDate = event.attributes?.startDate ? new Date(event.attributes?.startDate) : null
-    const endDate = event.attributes?.endDate ? new Date(event.attributes?.endDate) : null
+  const startDate = event.attributes?.startDate ? new Date(event.attributes?.startDate) : null
+  const endDate = event.attributes?.endDate ? new Date(event.attributes?.endDate) : null
+  const categories = event.attributes?.categories?.data ?? []
+  const hasCategories = categories?.length > 0
 
-    return (
-        <>
-            <Card className={""}>
-                <CardHeader>
-                    <CardTitle>{event.attributes?.title}</CardTitle>
-                    <CardDescription>
-                        <ScrollArea className="h-24 rounded">
-                            {event.attributes?.description}
-                        </ScrollArea>
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <div>
-                        <div>Start date</div>
-                        <div>{startDate?.formatDateTime(false)}</div>
-                    </div>
-                    <div>
-                        <div>End date</div>
-                        <div>{endDate?.formatDateTime(false)}</div>
-                    </div>
-                    <div>
-                        <div>Kategorie</div>
-                        <div>{event.attributes?.categories?.data?.map(c => c.attributes?.Name).join(", ")}</div>
-                    </div>
-                </CardContent>
-            </Card>
-        </>
-    )
+  return (
+    <>
+      <Card className={""}>
+        <CardHeader>
+          <CardTitle>{event.attributes?.title}</CardTitle>
+          <CardDescription>
+            <ScrollArea className="h-24 rounded">
+              {event.attributes?.description}
+            </ScrollArea>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className={"flex justify-between"}>
+            <div>Start date</div>
+            <div>{startDate?.formatDateTime(false)}</div>
+          </div>
+          <div className={"flex justify-between"}>
+            <div>End date</div>
+            <div>{endDate?.formatDateTime(false)}</div>
+          </div>
+          {hasCategories &&
+						<div className={"flex justify-between"}>
+							<div>Kategorie</div>
+							<div>{categories?.map(c => c.attributes?.Name).join(", ")}</div>
+						</div>
+          }
+        </CardContent>
+      </Card>
+    </>
+  )
 }
