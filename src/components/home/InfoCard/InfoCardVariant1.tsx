aae1a4811ee3348f6b2f54a5b948c9b3ec4fd15d
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2024-10-20 11:01:50 +0200

    update buttons to red

--- src/components/home/InfoCard/InfoCardVariant1.tsx
+++ src/components/home/InfoCard/InfoCardVariant1.tsx
@@ -8,12 +8,12 @@ interface Props {
 export const InfoCardVariant1 = ({card}: Props) => {
     console.log(card);
     return (
-        <div className="bg-white p-4 rounded-lg shadow-md">
+      <div className="flex flex-col bg-white p-4 gap-1 rounded-lg shadow-md">
             <h1 className="text-xl font-bold text-[#cd1421] text-center">{card.title}</h1>
             <p className="text-gray-500">{card.description}</p>
 
             {card.button?.url && card.button?.text && <div className={"flex justify-center"}>
-                <Button variant={"outline"}><a href={card.button?.url} target={"_blank"}>{card.button?.text}</a></Button>
+	            <Button variant={"destructive"}><a href={card.button?.url} target={"_blank"}>{card.button?.text}</a></Button>
             </div>}
         </div>
     );
