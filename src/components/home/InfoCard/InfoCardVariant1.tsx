8e47507fd12b58ce7e1936b2f906ba1f6fd58e9f
Jonas Faber <jonas.h.faber@gmail.com>
2024-10-20 13:40:59 +0200

    cleanup homepage

--- src/components/home/InfoCard/InfoCardVariant1.tsx
+++ src/components/home/InfoCard/InfoCardVariant1.tsx
@@ -1,5 +1,8 @@
 import {CardsInformationCardVariant1Component} from "@/endpoints/gubenProdSchemas";
 import {Button} from "@/components/ui/button";
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
 
 interface Props {
     card: CardsInformationCardVariant1Component;
@@ -8,8 +11,11 @@ interface Props {
 export const InfoCardVariant1 = ({card}: Props) => {
     return (
       <div className="flex flex-col bg-white p-4 gap-1 rounded-lg shadow-lg mb-4 break-inside-avoid">
-            <h1 className="text-xl font-bold text-[#cd1421] text-center">{card.title}</h1>
-            <p className="text-gray-500">{card.description}</p>
+            <img src={card.imgSrc} alt={card.imgAlt} className={"rounded"} />
+            <h1 className="text-xl font-bold text-gubenAccent text-center">{card.title}</h1>
+            <p className="text-gray-500">
+                <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>{card.description}</Markdown>
+            </p>
 
             {card.button?.url && card.button?.text && <div className={"flex justify-center"}>
 	            <Button variant={"destructive"}><a href={card.button?.url} target={"_blank"}>{card.button?.text}</a></Button>
